f81a5ac416313909ad63a624a8f13616ea1871b5
M.K. Safi <msafi@msafi.com>
2022-03-20 12:54:50 -0700

    Add plugins

--- schemas/schema.ts
+++ schemas/schema.ts
@@ -3,6 +3,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 import { celeb } from './celeb';
 import { fact } from './fact';
 import { issue } from './issue';
+// import { wildCard } from './wildCard';
 
 export default createSchema({
   name: 'hollowverse',
