c3337ee7780b65edae474afb53f7741adf33c6e5
M.K. Safi <msafi@msafi.com>
2022-03-31 12:52:05 -0700

    bug fix

--- app/schema/index.ts
+++ app/schema/index.ts
@@ -1,22 +1,18 @@
-import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
-
-// Import workflow types
-import workflowMetadata from './workflow/metadata';
-
+import createSchema from 'part:@sanity/base/schema-creator';
 // Import custom types
 import { celeb } from './celeb';
 import { fact } from './fact';
+import { orderOfTopics } from './orderOfTopics';
 import { tag } from './tag';
 import { topic } from './topic';
-import { orderOfTopics } from './orderOfTopics';
-import author from './author';
+// Import workflow types
+import workflowMetadata from './workflow/metadata';
 
 export default createSchema({
   name: 'hollowverse',
   // types: schemaTypes.concat([author, post, release, workflowMetadata]),
   types: schemaTypes.concat([
-    author,
     celeb,
     fact,
     tag,
