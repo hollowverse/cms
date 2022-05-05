e387929a29d345f054feeecacdb97e2d1f3cb128
M.K. Safi <msafi@msafi.com>
2022-05-05 08:03:23 -0700

    Levenshtein sorting

--- plugins/colr/KnowledgeGraph/index.tsx
+++ plugins/colr/KnowledgeGraph/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import { Card } from './Card';
 import { kgCall } from './kgCall';
+import { sort } from './sort';
 
 export function KnowledgeGraph() {
   const [value, setValue] = useState('');
@@ -15,40 +17,17 @@ export function KnowledgeGraph() {
           if (e.key === 'Enter') {
             const results = await kgCall(value);
 
-            setResults((await results.json()).itemListElement[0].result);
+            setResults((await results.json()).itemListElement);
           }
         }}
         type="text"
         placeholder="Search for a celebrity name"
       ></input>
 
-      {results && (
-        <div className="mt-5 p-3 bg-white rounded-xl">
-          <div className="flex w-full">
-            <img
-              src={results.image.contentUrl}
-              width={150}
-              className="rounded-lg"
-            />
-            <div className="ml-2">
-              <div className="p-3">
-                <h3 className="text-2xl">{results.name}</h3>
-                <span>{results.description}</span>
-              </div>
-              <div className="flex justify-between items-center p-3 bg-gray-200 rounded-lg">
-                <div className="mr-3">
-                  <span className="text-gray-400 block">
-                    Knowledge Graph ID
-                  </span>
-                  <span className="font-bold text-black text-xl">
-                    {results['@id']}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+      {results &&
+        sort(value, results).map((item) => (
+          <Card key={item.result['@id']} {...item.result} />
+        ))}
     </div>
   );
 }
